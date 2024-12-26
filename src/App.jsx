4173e99ad5470f32bc3a0a427b20364aed1fa2e0
Lohran Mendes <lohranfm@gmail.com>
2024-12-26 11:56:40 -0300

    chore: solved question - remove unsual folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const luana = {
+  cliente: 'Luana',
+  idade: 27,
+  compras: [
+    { nome: 'Notebook', preco: 'R$ 2500' },
+    { nome: 'Geladeira', preco: 'R$ 3000' },
+    { nome: 'Smartphone', preco: 'R$ 1500' }
+  ],
+  ativa: true
+}
 
-function App() {
-  const [count, setCount] = useState(0)
+const mario = {
+  cliente: 'Mario',
+  idade: 31,
+  compras: [
+    { nome: 'Notebook', preco: 'R$ 2500' },
+    { nome: 'Geladeira', preco: 'R$ 3000' },
+    { nome: 'Smartphone', preco: 'R$ 1500' },
+    { nome: 'Guitarra', preco: 'R$ 3500' }
+  ],
+  ativa: false
+}
+
+const App = () => {
+
+  function dados(pessoa) {
+    const totalGasto = (pessoa.compras.map((item) => Number(item.preco.replace("R$ ", "")))).reduce((a, b) => a + b);
+    return (
+      <>
+        <p>Nome: {pessoa.cliente}</p>
+        <p>Idade: {pessoa.idade}</p>
+        <p>Situação: <span style={{ color: pessoa.ativa ? "green" : "red" }}>{pessoa.ativa ? 'Ativo' : 'Inativo'}</span></p>
+        <p>Total gasto: R${totalGasto}</p>
+        {totalGasto > 10000 && <p>Você está gastando muito!</p>}
+      </>
+    )
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    dados(luana)
   )
 }
 
